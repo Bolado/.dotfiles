ed353778d1cf9899571d6c322a5f55d9db946d24
Bolado <github@bolado.dev>
2025-06-26 02:57:51 +0200

    Match fish prompt with my ghostty theme

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -2,25 +2,26 @@ function fish_prompt
         if not set -q VIRTUAL_ENV_DISABLE_PROMPT
                 set -g VIRTUAL_ENV_DISABLE_PROMPT true
         end
-        set_color yellow
+        set_color DC143C
         printf '%s' $USER
-        set_color normal
+        set_color 696969
         printf ' at '
-    
-        set_color magenta
+
+        set_color 696969
         echo -n (prompt_hostname)
-        set_color normal
+        set_color 696969
         printf ' in '
-    
-        set_color $fish_color_cwd
+
+        set_color C0C0C0
         printf '%s' (prompt_pwd)
         set_color normal
-    
+
         # Line 2
         echo
         if test -n "$VIRTUAL_ENV"
-                printf "(%s) " (set_color blue)(path basename $VIRTUAL_ENV)(set_color normal)
+                printf "(%s) " (set_color FF4500)(path basename $VIRTUAL_ENV)(set_color normal)
         end
+        set_color DC143C
         printf '↪ '
         set_color normal
 end
