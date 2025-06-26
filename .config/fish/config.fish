213873f0dd41d1c61b8a20bb661c6a5ad8cf0bb1
Bolado <github@bolado.dev>
2025-06-26 03:27:26 +0200

    Added fastfetch to fish greetings and .hushlogin creation

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,8 @@
 source ~/.config/fish/aliases.fish
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# create .hushlogin file if it doesn't exist to remove login message next time
+if not test -f ~/.hushlogin
+    touch ~/.hushlogin
+end
